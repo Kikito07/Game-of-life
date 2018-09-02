5e6001644ebe28cfcf4db68b84b086287a7e2d88
Kikito <nikita.tyunyayev@student.uclouvain.be>
2018-09-02 20:11:25 +0200

    ajoute zoneTest

---
+++ zoneTest.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+def addcell(arrayTest, x, y):
+    arrayTest[x, y] = 1
+    return
+
+
+arrayTest = np.zeros((10, 10))
+addcell(arrayTest, 3, 5)
+addcell(arrayTest, 3, 6)
+addcell(arrayTest, 3, 7)
+addcell(arrayTest, 4, 5)
+addcell(arrayTest, 4, 6)
+addcell(arrayTest, 4, 7)
+
+
+def animate(data):
+    global arrayTest
+    arrayNew = np.zeros(arrayTest[0, :].size, arrayTest[0, :].size)
+
+    for row in range(arrayTest[0, :].size):
+        for column in range(arrayTest[0, :].size):
+
+            if (arrayTest[row, column] == 1):
+                total = 0
+                for rangRow in range(-1, 2):
+                    for rangeColumn in range(-1, 2):
+                        if (row + rangRow < (arrayTest[0, :].size) and row + rangRow >= 0 and
+                                column + rangeColumn < (arrayTest[0, :].size) and column + rangeColumn >= 0):
+                            total = total + arrayTest[row + rangRow, column + rangeColumn]
+                if total == 4 or total == 3:
+                    arrayNew[row, column] = 1
+
+            else:
+                total = 0
+                for rangRow in range(-1, 2):
+                    for rangeColumn in range(-1, 2):
+                        if (row + rangRow < (arrayTest[0, :].size) and row + rangRow >= 0 and
+                                column + rangeColumn < (arrayTest[0, :].size) and column + rangeColumn >= 0):
+                            total = total + arrayTest[row + rangRow, column + rangeColumn]
+                if total == 3:
+                    arrayNew[row, column] = 1
+
+    mat.set_data(arrayNew)
+    arrayTest = arrayNew
+    return [mat]
+
+
+fig, ax = plt.subplots()
+mat = ax.matshow(arrayTest)
+ani = animation.FuncAnimation(fig, animate, interval=50, save_count=50)
